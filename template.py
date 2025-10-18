2b81de3e7ff1c1553a63ac9134ff38319bb50a30
SameenAdil <sameenkainaat1512@gmail.com>
2025-10-18 14:57:06 -0700

    requirements added

--- template.py
+++ template.py
@@ -19,7 +19,7 @@ list_of_files = [
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
-    "requirements.yaml",
+    "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
