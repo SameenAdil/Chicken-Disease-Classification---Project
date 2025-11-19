89ee2d75ab47c48b0c50bbf92abb58e842b54835
SameenAdil <sameenkainaat1512@gmail.com>
2025-11-18 20:34:01 -0800

    evaluation added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipline.stage_04_evaluation import EvaluationPipeline
 
 
 #logger.info("welocme o my custom log")   #custom logger
@@ -38,6 +39,20 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
